a1feec72a1099ac07dac1a47e312d4494ff09615
PavloPinchuk <71881450+PavloPinchuk@users.noreply.github.com>
2021-11-07 13:16:27 +0200

    Pinchuk Pavlo HW_10 Part_1 (HW_8 remake)

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.univ;
+
+import com.univ.Task_3_8.Task_3_8_main;
+import com.univ.Task_4_15.Task_4_15_main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Task_3_8_main.main();
+        Task_4_15_main.main();
+    }
+}
