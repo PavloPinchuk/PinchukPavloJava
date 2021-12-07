ff61ba9a78fe053f92799caec36882431a05515f
PavloPinchuk <71881450+PavloPinchuk@users.noreply.github.com>
2021-12-07 10:46:27 +0200

    Pinchuk Pavlo Lab_13

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.univ;
+
+import com.univ.Task_1.Task_1_main;
+import com.univ.Task_2.Task_2_main;
+import com.univ.Task_3.Task_3_main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Task_1_main.main();
+        //Task_2_main.main();
+        Task_3_main.main();
+    }
+}
